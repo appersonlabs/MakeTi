4cdebd4beb57c0c29934996aa56d1e2e41a8a4c5
Matt <me@mattapperson.com>
2011-12-31 21:59:43 -0500

    better error messages

--- bin/titanium.sh
+++ bin/titanium.sh
@@ -91,12 +91,13 @@ elif [ ${APP_DEVICE} == "android" ]; then
 
 	bash -c "'${TI_ANDROID_BUILD}' simulator ${APP_NAME}  ${ANDROID_SDK_PATH} ${PROJECT_ROOT}/ ${APP_ID} ${android}" \
 	| perl -pe 's/^\[DEBUG\].*$/\e[35m$&\e[0m/g;s/^\[INFO\].*$/\e[36m$&\e[0m/g;s/^\[WARN\].*$/\e[33m$&\e[0m/g;s/^\[ERROR\].*$/\e[31m$&\e[0m/g;'
+
 elif [ ${APP_DEVICE} == "web" ]; then
 
 	echo "[ERROR] This SDK is not yet supported"
 	exit 1
 
 else
-	echo "[ERROR] not supported!"
+	echo "[ERROR] platform ${APP_DEVICE} is not supported!"
 	echo ${APP_DEVICE}
 fi
