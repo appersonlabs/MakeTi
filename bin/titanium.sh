d55960e4281e41e1ac241bff7363b9e338e52d0e
Matt <me@mattapperson.com>
2012-01-24 12:16:31 -0500

    added debugging line

--- bin/titanium.sh
+++ bin/titanium.sh
@@ -134,7 +134,7 @@ if [ ${APP_DEVICE} == "iphone" -o ${APP_DEVICE} == "ipad" ]; then
 				temp_array=(${line//{\"uuid\": \"/})
 
 				UUID=${temp_array[0]//\"/}
-
+                echo "'${TI_IPHONE_BUILD}' install ${iphone} '${PROJECT_ROOT}/' $(echo $APP_ID) '$(echo $APP_NAME)' '$(echo $UUID | sed -e "s/uuid: //g")' '${SIGNING_IDENTITY}'"
 				bash -c "'${TI_IPHONE_BUILD}' install ${iphone} '${PROJECT_ROOT}/' $(echo $APP_ID) '$(echo $APP_NAME)' '$(echo $UUID | sed -e "s/uuid: //g")' '${SIGNING_IDENTITY}'" | \
 				while read build_log
 				do
